10cf84f3544dbeed3e9ca1e55ab1193edf83dcc8
George Hlongwane <georgehlongwane8@gmail.com>
2025-07-20 22:15:15 +0200

    Created a model for the customer persona

--- persona_generator/personas/models.py
+++ persona_generator/personas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class CustomerPersona(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    occupation = models.CharField(max_length=100)
+    interests = models.TextField()
+    challenges = models.TextField()
+    goals = models.TextField()
+
+    def __str__(self):
+        return self.name
